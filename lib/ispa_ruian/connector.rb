d250feae33ce0a8398675a1128f74a62722f55f1
Jan Schejbal <honza@arius.cz>
2017-01-10 10:42:46 +0100

    fix post method

--- lib/ispa_ruian/connector.rb
+++ lib/ispa_ruian/connector.rb
@@ -11,7 +11,7 @@ module IspaRuian
       connector.get
     end
     
-    def self.post(url_params, post_data, options = {})
+    def self.post(params, post_data, options = {})
       connector = Connector.new(params, options)
       connector.post post_data
     end
@@ -35,7 +35,7 @@ module IspaRuian
       raise e
     end
     
-    def post(url_params, post_data, options = {})
+    def post(post_data)
 
       @curl.http_post(post_data.to_json) do |curl|
         curl = set_headers(curl)
@@ -73,7 +73,6 @@ module IspaRuian
     def build_url(params, options)
       options.merge!(expanded: 1)
       encoded_uri = URI.encode(([Configuration.api_url] + params).flatten.compact.join("/") + "?" + options.to_query)
-      p encoded_uri
       return encoded_uri
     end
     
